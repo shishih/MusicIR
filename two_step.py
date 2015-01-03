a1c6b9415effd8dfb2a65af16ddbb876e97de98a
SLAPaper <slapaper.pku@gmail.com>
2015-01-03 21:00:56 +0800

    two step clustering finished, doing with output

--- two_step.py
+++ two_step.py
@@ -4,6 +4,8 @@ import math
 import functools
 import calc_cos as c
 
+count = 0
+
 def get_mean(v1,v2):
     if len(v1) != len(v2):
         print('the length of the two vector dosn\'t match')
@@ -11,12 +13,12 @@ def get_mean(v1,v2):
     length = len(v1)
     mean = {}
     for i in range(1,length):
-        mean[0] = 'null'
         mean[i] = (float(v1[i]) + float(v2[i])) / 2
     return mean
 
 class step:
     def __init__(self,args):
+        global count
         if len(args) == 1:
             self.left = None
             self.right = None
@@ -30,6 +32,8 @@ class step:
                 self.me = get_mean(left.me,right.me)
         else:
             raise TypeError
+        self.me[0] = count
+        count += 1
 
 FILE = './a2.txt'
 
@@ -45,13 +49,20 @@ for s in df:
         ltemp[i] = stemp[i].split(':')[1]
     l.append(step((ltemp,)))
 
+t = {}
 while len(l) > 1:
     length = len(l)
     min_pair = [0,1]
     max_cos = 0
     for i in range(length):
+        if l[i].me[0] not in t:
+            t[l[i].me[0]] = {}
         for j in range(i + 1,length):
-            cos = c.calc_cos(l[i].me,l[j].me)
+            if l[j].me[0] in t[l[i].me[0]]:
+                cos = t[l[i].me[0]][l[j].me[0]]
+            else:
+                cos = c.calc_cos(l[i].me,l[j].me)
+                t[l[i].me[0]][l[j].me[0]] = cos
             if cos > max_cos:
                 max_cos = cos
                 min_pair = [i,j]
